6a99390dc888a1cc4a940d8b9b2ef199967e6ec1
wendylin <wendylw2013@gmail.com>
2018-03-15 02:17:58 +0800

    add news

--- js/main.js
+++ js/main.js
@@ -11,4 +11,6 @@ $(document).ready(function() {
   window.onload = function () {
     $('.loading').addClass('hidden');
   };
+
+  $('.s2__bg-circle').css('background-image', 'url(./images/circle-3-4-5.gif)');
 })()
