6f4ab4fbc384e8e5252cb7b958ee34010dfbad99
wendylin <wendylw2013@gmail.com>
2018-03-24 23:46:03 +0800

    dev: new frame

--- old/sendmail.php
+++ old/sendmail.php
@@ -1,26 +1,31 @@
 <?php
 
-	$receiver = $_POST['receiver'] ?? '';          //PROVIDE YOUR EMAIL ADDRESS IN HTML CONTACT FORM SECTION CHANGE value="admin@mail.com"
-	$subject = $_POST['subject'] ?? '';            //PROVIDE THE SUBJECT OF THE EMAIL IN HTML CONTACT FORM SECTION CHANGE value="contactform"
+$receiver = $_POST['receiver'];          //PROVIDE YOUR EMAIL ADDRESS IN HTML CONTACT FORM SECTION CHANGE value="admin@mail.com"
+$subject = $_POST['subject'];            //PROVIDE THE SUBJECT OF THE EMAIL IN HTML CONTACT FORM SECTION CHANGE value="contactform"
 
 
-	$email = $_POST['email'] ?? '';
+$email = $_POST['email'];
 
-	$message = "<br/>Email: " . $email ;
+$message = "<br/>Email: " . $email ;
 
-	$message .= "<br/><br/><br/>Date: " . date("Y-m-d h:i:s");
+$message .= "<br/><br/><br/>Date: " . date("Y-m-d h:i:s");
 
-	$siteEmail = $receiver;
-	$emailTitle = $subject;
+$siteEmail = $receiver;
+$emailTitle = $subject;
 
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .='From: ' . ' <' . $email . '>';
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .='From: ' . ' <' . $email . '>';
 
-	if(mail($receiver, $emailTitle, $message, $headers)){
-		echo 'success';
-	} else {
-		echo 'error';
-	}
+if($_POST['website_url'] == '')
+{
+	if(mail($receiver, $emailTitle, $message, $headers))
+		{ echo 'success'; }
+	else { echo 'error'; }
+}
+else
+{
+	echo 'error';
+}
 
 ?>
