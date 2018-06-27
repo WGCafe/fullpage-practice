3a62ba2cbc5401a8d8da5beead231e3ddd0a01b0
grace.yang <grace.yang@otms.com>
2018-06-28 00:41:05 +0800

    Add the contact form validation

--- js/main.js
+++ js/main.js
@@ -7,8 +7,13 @@ function isValidEmailAddress(emailAddress) {
   return pattern.test(emailAddress);
 }
 
+function isValidPhoneNumber(phoneNumber) {
+  var pattern = new RegExp("^[0-9]*$");
+  return pattern.test(phoneNumber);
+}
+
 $(function() {
-  $('#subscribeSubmit').on("click", function() {
+  $('#subscribeSubmit').on('click', function() {
     var inputValue = $('#subscribeEmail').val();
 
     $('.subscribe__email-validate-error').remove();
@@ -22,5 +27,70 @@ $(function() {
     }
   });
 
+  $('#contactSubmit').on('click', function() {
+    var $parent = $(this).parent();
+    var _inputs = $('.contact-us :input');
+    
+    var errorMsg = '';
 
+    $parent.find(".formtips").remove();
+
+    
+    // Validate contact name
+    if( _inputs.is('#contactName')){
+      var _this = $('#contactName');
+
+      if(!_this.val() || _this.val() === ''){
+        errorMsg = '请输入至少用户名.';
+        $('#contactName').parent().append('<span class="formtips onError">'+errorMsg+'</span>');
+      }
+    }
+
+    // Validate contact phone
+    if(_inputs.is('#contactPhone')){
+      var _this = $('#contactPhone');
+      
+      if(!_this.val() || _this.val() === ''){
+        errorMsg = 'Please enter the contact phone.';
+      } else if (!isValidPhoneNumber(_this.val())) {
+        errorMsg = 'The contact phone should be number.';
+      }
+
+      $('#contactPhone').parent().append('<span class="formtips onError">'+errorMsg+'</span>');      
+    }
+
+    // Validate contact email
+    if(_inputs.is('#contactEmail')){
+      var _this = $('#contactEmail');
+      
+      if(!_this.val() || _this.val() === ''){
+        errorMsg = 'Please enter the contact email.';
+      } else if (!isValidEmailAddress(_this.val())) {
+        errorMsg = 'Please enter correct mailbox.';
+      }
+
+      $('#contactEmail').parent().append('<span class="formtips onError">'+errorMsg+'</span>');
+    }
+
+     // Validate contact address
+     if( _inputs.is('#contactAddress')){
+      var _this = $('#contactAddress');
+       
+      if(!_this.val() || _this.val() === ''){
+        errorMsg = 'Please enter the contact address.';
+        $('#contactAddress').parent().append('<span class="formtips onError">'+errorMsg+'</span>');
+      }
+    }
+
+    // Validate contact message
+    var message = $('#contactMessage'); 
+    if(!message.val() || message.val() === ''){
+      errorMsg = 'Please enter the contact message.';
+      $('#contactMessage').parent().append('<span class="formtips onError">'+errorMsg+'</span>');
+    }
+
+    if (errorMsg) {
+      return false;
+    }
+  });
 });
